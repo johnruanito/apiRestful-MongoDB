958533472528c41aff703f9a399ec48623d6fc1e
jonhruanito <ruanrpbp@gmail.com>
2024-07-05 14:45:07 -0300

    init commit

---
+++ src/main/java/com/rpbp/apiRestMongo/ApiRestMongoApplication.java
@@ -0,0 +1,13 @@
+package com.rpbp.apiRestMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpbp/apiRestMongo/ApiRestMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpbp.apiRestMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
